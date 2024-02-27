a0288e30be86968c33777fec1a04a66563e8931c
Daniel <oscar.d.gonzalez@gmail.com>
2024-02-27 17:58:01 -0500

    test: :white_check_mark: 'ProductItem' unit test suite

---
+++ __tests__/unit/productItem.test.js
@@ -0,0 +1,97 @@
+import { ProductItem } from "../../js/components/productItem";
+
+describe("ProductItem", () => {
+  let productItem, image, alt, name, price;
+
+  beforeEach(() => {
+    // Arrange
+    image = 'image.jpg';
+    alt = 'alt text';
+    name = 'Product Name';
+    price = 1000;
+
+    // Act
+    productItem = new ProductItem({ image, alt, name, price });
+    document.body.appendChild(productItem);
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    document.body.removeChild(productItem);
+  })
+
+  test('should create ProductItem element with correct properties', () => {
+    // Assert
+    expect(productItem.image).toBe(image);
+    expect(productItem.alt).toBe(alt);
+    expect(productItem.name).toBe(name);
+    expect(productItem.price).toBe(price);
+  });
+
+  test('should render the correct HTML', () => {
+    // Arrange
+    // Mock the numberToPrice function
+    const mockNumberToPrice = jest.fn();
+    mockNumberToPrice.mockReturnValue('$10.00');
+
+    // Assert
+    expect(productItem.innerHTML).toBe(`
+      <li data-name="Product Name">
+        <div class="plate">
+          <img src="images/image.jpg" alt="alt text" class="plate">
+        </div>
+        <div class="content">
+          <p class="menu-item">Product Name</p>
+          <p class="price">$10.00</p>
+          <button class="add">Add to cart</button>
+        </div>
+      </li>
+    `);
+  })
+
+  test('Should dispatch a custom event with correct details when \'Add To Cart\' button is clicked', () => {
+    // Arrange
+    // Spy on the dispatchEvent function
+    const dispatchEventSpy = jest.spyOn(document, 'dispatchEvent');
+
+    // Act
+    const button = productItem.querySelector('button');
+    button.click();
+
+    // Assert
+    expect(dispatchEventSpy).toHaveBeenCalledWith(new CustomEvent('add-to-cart', {
+      detail: {
+        name: 'Product Name',
+        image: 'image.jpg',
+        alt: 'alt text',
+        price: 1000,
+      }}
+    ))
+  })
+
+  test('Should update button text/content and class when \'Add To Cart\' button is clicked',() => {
+    // Act
+    const button = productItem.querySelector('button');
+    productItem.update();
+
+    // Assert
+    expect(button.classList.contains('add')).toBe(false);
+    expect(button.classList.contains('in-cart')).toBe(true);
+    expect(button.querySelector('img').src).toContain('images/check.svg');
+    expect(button.querySelector('img').alt).toBe('check');
+    expect(button.innerText).toBe('In Cart');
+  })
+
+  test('Should remove the event listener when \'ProductItem\' element is removed from the DOM', () => {
+    // Arrange
+    const button = productItem.querySelector('button');
+    // Spy on the removeEventListener function
+    const removeEventListenerSpy = jest.spyOn(button, 'removeEventListener');
+
+    // Act
+    productItem.disconnectedCallback();
+
+    // Assert
+    expect(removeEventListenerSpy).toHaveBeenCalledWith('click', expect.any(Function))
+  })
+})
